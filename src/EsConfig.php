8b27cb14e6a3e4eec14f83f36e7ab000f86ef8f2
Pablo de la Peña <p@shedcollective.org>
2019-09-26 13:14:01 +0100

    Bumps version number

--- src/EsConfig.php
+++ src/EsConfig.php
@@ -4,7 +4,7 @@ namespace EsConfig;
 
 class EsConfig
 {
-    const VERSION             = '2.2.1';
+    const VERSION             = '2.3.0';
     const CONFIG_FILE         = '.esconfig.json';
     const ENVIRONMENT_FILE    = '.esconfig.environment';
     const DEFAULT_ENVIRONMENT = 'DEVELOPMENT';
